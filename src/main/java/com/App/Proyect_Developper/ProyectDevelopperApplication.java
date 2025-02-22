fd42ed88ac64f463401c85d5235ef72c08c68d23
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-22 16:10:22 -0500

    Git - Update of the Proyect

---
+++ src/main/java/com/App/Proyect_Developper/ProyectDevelopperApplication.java
@@ -0,0 +1,13 @@
+package com.App.Proyect_Developper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectDevelopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectDevelopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/App/Proyect_Developper/ProyectDevelopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.Proyect_Developper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectDevelopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
