a7c84e0d8840b56ebbde5cd31c3131c1e9bcc8ad
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-22 16:25:14 -0500

    Update All - Spring Segurity

--- src/main/java/com/App/Proyect_Developper/Configuration/ConfigurationSegurity.java
+++ src/main/java/com/App/Proyect_Developper/Configuration/ConfigurationSegurity.java
@@ -1,4 +1,9 @@
 package com.App.Proyect_Developper.Configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class ConfigurationSegurity {
 }
