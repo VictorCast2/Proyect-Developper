5c3768c3e2b931e103f6b5ed796c933e22348df4
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-23 00:50:11 -0500

    Update Model

--- src/main/java/com/App/Proyect_Developper/Model/UserModel.java
+++ src/main/java/com/App/Proyect_Developper/Model/UserModel.java
@@ -3,8 +3,10 @@ package com.App.Proyect_Developper.Model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 @Entity
@@ -30,12 +32,14 @@ public class UserModel implements UserDetails {
     private String Contrasenna;
 
     @Column(name = "Roles", nullable = false)
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "UserId", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<RolesModel> roles = new HashSet<>();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles.stream()
+                .map(rol -> new SimpleGrantedAuthority(rol.getRol()))
+                .collect(Collectors.toList());
     }
 
     @Override
